b39ddca9f8a5089ffe7f4857c44e05a2f393a6be
Ludwig Richter <ludwig.richter@stud-mail.uni-wuerzburg.de>
2021-07-31 11:44:57 +0200

    feat: Import current implementation from https://github.com/wuespace/telestion-project-daedalus2
    Co-authored-by: jantischhoefer <jan.tischhoefer@gmail.com>

---
+++ src/main/java/de/wuespace/telestion/extension/mongodb/DbResponse.java
@@ -0,0 +1,15 @@
+package de.wuespace.telestion.extension.mongodb;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import de.wuespace.telestion.api.message.JsonMessage;
+import io.vertx.core.json.JsonObject;
+
+import java.util.Collections;
+import java.util.List;
+
+public record DbResponse(
+		@JsonProperty List<JsonObject> result) implements JsonMessage {
+			private DbResponse() {
+				this(Collections.emptyList());
+			}
+}
