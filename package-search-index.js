2b4f5122c8698b8af195bae1b6e030a40b43a4f7
fussel178 <richter@fliegwerk.com>
2022-01-27 12:34:54 +0000

    Deploying to gh-pages from @ wuespace/telestion-extension-mongodb@1a46dae1510c9443d16c8edc38ec5ce36228c90b 🚀

--- package-search-index.js
+++ package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"de.wuespace.telestion.extension.mongodb"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"de.wuespace.telestion.extension.mongodb"},{"l":"de.wuespace.telestion.extension.mongodb.message"},{"l":"de.wuespace.telestion.extension.mongodb.util"}];updateSearchResults();
